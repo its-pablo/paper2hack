ac773e47372c0d00704fa1e043374f148222ccb9
its-pablo <pablobooks67@gmail.com>
2022-03-05 20:38:06 -0800

    no more iife

--- dist/paper2hack.user.js
+++ dist/paper2hack.user.js
@@ -1,38 +1,41 @@
 // ==UserScript==
-// @name         paper2hack
+// @name         paper2hack beta
 // @description  Modding utility/menu for paper.io
 // @version      0.1.2
 // @author       its-pablo
-// @match        https://paper-io.com/
+// @match        https://paper-io.com//
 // @icon         https://paper-io.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
-(function () {
+window.addEventListener("DOMContentLoaded", function () {
     'use strict';
+    //setup
     let head = document.getElementsByTagName('head')[0];
     let ui = document.createElement('script')
     ui.setAttribute('src', '//code.jquery.com/ui/1.13.1/jquery-ui.js')
-    //head.appendChild(ui)
+    head.appendChild(ui)
+    alert('jquery-ui loaded')
     let overlayHTML = `
-    <div id="box">
-        <button class="ou" id="accordian">paper2hack v1</button>
-        <div class="ou" id="box2">
+<div id="box">
+    <button class="ou" id="accordian">paper2hack v1</button>
+    <div class="ou" id="box2">
 
-            <section><label>Scroll to zoom</label><input id="zooming" type="checkbox"></section>
-            <section><label>Debug menu</label><input id="debugCtx" type="checkbox"></section>
-            <section>
-                <div class="dropdown"><p>Skin (hover)</p></div>
-            </section>
-            <section><label>Speed</label><input id="unitSpeed" type="number"></section>
-            <section><label>Arena Size</label><input id="arenaSize" type="number"></section>
-            <section><label>Quad Size</label><input id="quadSize" type="number"></section>
-            <section><label>Bots count</label><input id="botsCount" type="number"></section>
-            <section><div class="dropdown"><button class="dropbtn" id="buttonUnlock">Unlock all skins</button></section>
-            <section><div class="dropdown"><button class="dropbtn" id="button play">Start Game</button></section>
-            <section><sub>You can hide the menu with Ctrl+B</sub></section>
-        </div>
-    </div>
+        <section><label>Scroll to zoom</label><input id="zooming" type="checkbox"></section>
+        <section><label>Debug menu</label><input id="debugCtx" type="checkbox"></section>
+        <section>
+        <p>Skin (hover)</p><div class="dropdown"></div>
+        </section>
+        <section><label>Speed</label><input id="unitSpeed" type="number"></section>
+        <section><label>Arena Size</label><input id="arenaSize" type="number"></section>
+        <section><label>Quad Size</label><input id="quadSize" type="number"></section>
+        <section><label>Bots count</label><input id="botsCount" type="number"></section>
+        <section><div class="dropdown"><button class="dropbtn" id="buttonUnlock">Unlock all skins</button></section>
+        <section><div class="dropdown"><button class="dropbtn" id="button play" onclick="game_start()">Start Game</button></section>
+        <section><sub>You can hide the menu with Ctrl+B</sub></section>
+     </div>
+</div>
+
 <style>
 #box {
     z-index: 10;
@@ -90,39 +93,12 @@ input[type=color] { width: 50px;}
 
 .dropdown {
   position: relative;
+  display: none;
+}
+
+.dropdown:hover {
   display: inline-block;
 }
-
-.custom-button p {
-  color: white;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown-content p:hover {background-color: #121414;}
-
-.dropdown-content1 p:hover {background-color: #121414;}
-
-.dropdown-content2 p:hover {background-color: #121414;}
-
-.dropdown-content3 p:hover {background-color: #121414;}
-
-.dropdown-content4 p:hover {background-color: #121414;}
-
-.custom-button p:hover {background-color: #121414;}
-
-.dropdown:hover .dropdown-content {display: block; background-color: #242829;}
-
-.dropdown:hover .dropdown-content1 {display: block; background-color: #242829;}
-
-.dropdown:hover .dropdown-content2 {display: block; background-color: #242829;}
-
-.dropdown:hover .dropdown-content3 {display: block; background-color: #242829;}
-
-.dropdown:hover .dropdown-content4 {display: block; background-color: #242829;}
-
-.dropdown:hover .dropbtn {background-color: #121414;}
 </style>
 `
     //$('#box').draggable()
@@ -331,247 +307,12 @@ input[type=color] { width: 50px;}
         drpdwn.appendChild(el)
         skins[i].element = el;
     })
-    function get(x) { return document.getElementById(x) };
-
 
     // Setting up the html div
     let overlay = document.createElement("div");
     overlay.innerHTML = overlayHTML;
     document.body.appendChild(overlay);
-
-    // Getting variables from div by id's
-    let acc = get("accordian"),
-        sb1 = get("skinbtn1"),
-        sb2 = get("skinbtn2"),
-        sb3 = get("skinbtn3"),
-        sb4 = get("skinbtn4"),
-        sb5 = get("skinbtn5"),
-        sb6 = get("skinbtn6"),
-        sb7 = get("skinbtn7"),
-        sb8 = get("skinbtn8"),
-        sb9 = get("skinbtn9"),
-        sb10 = get("skinbtn10"),
-        sb11 = get("skinbtn11"),
-        sb12 = get("skinbtn12"),
-        sb13 = get("skinbtn13"),
-        sb14 = get("skinbtn14"),
-        sb15 = get("skinbtn15"),
-        sb16 = get("skinbtn16"),
-        sb17 = get("skinbtn17"),
-        sb18 = get("skinbtn18"),
-        sb19 = get("skinbtn19"),
-        sb20 = get("skinbtn20"),
-        sb21 = get("skinbtn21"),
-        sb22 = get("skinbtn22"),
-        sb23 = get("skinbtn23"),
-        sb24 = get("skinbtn24"),
-        sb25 = get("skinbtn25"),
-        sb26 = get("skinbtn26"),
-        sb27 = get("skinbtn27"),
-        sb28 = get("skinbtn28"),
-        sb29 = get("skinbtn29"),
-        sb30 = get("skinbtn30"),
-        sb31 = get("skinbtn31"),
-        sb32 = get("skinbtn32"),
-        sb33 = get("skinbtn33"),
-        sb34 = get("skinbtn34"),
-        sb35 = get("skinbtn35"),
-        sb36 = get("skinbtn36"),
-        sb37 = get("skinbtn37"),
-        sb38 = get("skinbtn38"),
-        sb39 = get("skinbtn39"),
-        sb40 = get("skinbtn40"),
-        buttonplay = get("button play"),
-        hackroyale = get("hackedRoyale"),
-        testchange = get("testChange");
-
-
-    //button functions
-
-    //WARNING: Skins 31-34 will not work
-
-    //first menu
-
-
-    sb1.onclick = function () {
-        document.cookie = "skin=skin_00"
-        location.reload();
-    }
-    sb2.onclick = function () {
-        document.cookie = "skin=skin_01"
-        location.reload();
-    }
-    sb3.onclick = function () {
-        document.cookie = "skin=skin_02"
-        location.reload();
-    }
-    sb4.onclick = function () {
-        document.cookie = "skin=skin_03"
-        location.reload();
-    }
-    sb5.onclick = function () {
-        document.cookie = "skin=skin_04"
-        location.reload();
-    }
-    sb6.onclick = function () {
-        document.cookie = "skin=skin_05"
-        location.reload();
-    }
-    sb7.onclick = function () {
-        document.cookie = "skin=skin_06"
-        location.reload();
-    }
-    sb8.onclick = function () {
-        document.cookie = "skin=skin_07"
-        location.reload();
-    }
-    sb9.onclick = function () {
-        document.cookie = "skin=skin_08"
-        location.reload();
-    }
-
-
-    //second menu
-
-
-    sb10.onclick = function () {
-        document.cookie = "skin=skin_09"
-        location.reload();
-    }
-    sb11.onclick = function () {
-        document.cookie = "skin=skin_10"
-        location.reload();
-    }
-    sb12.onclick = function () {
-        document.cookie = "skin=skin_11"
-        location.reload();
-    }
-    sb13.onclick = function () {
-        document.cookie = "skin=skin_12"
-        location.reload();
-    }
-    sb14.onclick = function () {
-        document.cookie = "skin=skin_13"
-        location.reload();
-    }
-    sb15.onclick = function () {
-        document.cookie = "skin=skin_14"
-        location.reload();
-    }
-    sb16.onclick = function () {
-        document.cookie = "skin=skin_15"
-        location.reload();
-    }
-    sb17.onclick = function () {
-        document.cookie = "skin=skin_16"
-        location.reload();
-    }
-
-
-    //third menu
-
-    sb18.onclick = function () {
-        document.cookie = "skin=skin_17"
-        location.reload();
-    }
-    sb19.onclick = function () {
-        document.cookie = "skin=skin_18"
-        location.reload();
-    }
-    sb20.onclick = function () {
-        document.cookie = "skin=skin_19"
-        location.reload();
-    }
-    sb21.onclick = function () {
-        document.cookie = "skin=skin_20"
-        location.reload();
-    }
-    sb22.onclick = function () {
-        document.cookie = "skin=skin_21"
-        location.reload();
-    }
-    sb23.onclick = function () {
-        document.cookie = "skin=skin_22"
-        location.reload();
-    }
-    sb24.onclick = function () {
-        document.cookie = "skin=skin_23"
-        location.reload();
-    }
-    sb25.onclick = function () {
-        document.cookie = "skin=skin_24"
-        location.reload();
-    }
-
-
-    //fourth menu
-
-
-    sb26.onclick = function () {
-        document.cookie = "skin=skin_25"
-        location.reload();
-    }
-    sb27.onclick = function () {
-        document.cookie = "skin=skin_26"
-        location.reload();
-    }
-    sb28.onclick = function () {
-        document.cookie = "skin=skin_27"
-        location.reload();
-    }
-    sb29.onclick = function () {
-        document.cookie = "skin=skin_28"
-        location.reload();
-    }
-    sb30.onclick = function () {
-        document.cookie = "skin=skin_29"
-        location.reload();
-    }
-    sb31.onclick = function () {
-        document.cookie = "skin=skin_30"
-        location.reload();
-    }
-    sb32.onclick = function () {
-        document.cookie = "skin=skin_35"
-        location.reload();
-    }
-    sb33.onclick = function () {
-        document.cookie = "skin=skin_36"
-        location.reload();
-    }
-
-
-    //fifth menu
-
-
-    sb34.onclick = function () {
-        document.cookie = "skin=skin_37"
-        location.reload();
-    }
-    sb35.onclick = function () {
-        document.cookie = "skin=skin_38"
-        location.reload();
-    }
-    sb36.onclick = function () {
-        document.cookie = "skin=skin_39"
-        location.reload();
-    }
-    sb37.onclick = function () {
-        document.cookie = "skin=skin_40"
-        location.reload();
-    }
-    sb38.onclick = function () {
-        document.cookie = "skin=skin_42" //41 breaks
-        location.reload();
-    }
-    sb39.onclick = function () {
-        document.cookie = "skin=skin_43"
-        location.reload();
-    }
-    buttonplay.onclick = function () {
-        game_start();
-
-    }
+    $('#box').draggable()
 
     //Hide/show menu with keyboard shortcut for streamers
     document.onkeydown = function (e) {
@@ -630,4 +371,4 @@ input[type=color] { width: 50px;}
 
     })
 
-})();
+});
